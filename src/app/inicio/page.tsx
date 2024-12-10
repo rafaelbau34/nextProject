00768531435eef9951a9b328362845f91cdef902
rafael <rafaelbaugar@gmail.com>
2024-12-10 03:03:32 -0600

    ya acabé

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import Image from "next/image";
